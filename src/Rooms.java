f3ee88797b0101978d8bea7846d4cbf0c467319b
Revodavid1 <32637870+Revodavid1@users.noreply.github.com>
2017-11-15 20:58:05 -0500

    Update Rooms.java
    added room exits to constructor, and methods to return the room exits.

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,12 +1,16 @@
 package lostinpandora;
 
 public class Rooms {
-	String name;
-	String description;
+	String name,description;
+	int exitNorth, exitSouth,exitWest,exitEast;
 	
-	public Rooms(String name, String description) {
+	public Rooms(String name, String description, int exitNorth, int exitSouth,int exitWest,int exitEast) {
 		this.name = name;
-		this.description = description;	
+		this.description = description;
+		this.exitNorth = exitNorth;
+		this.exitSouth = exitSouth;
+		this.exitWest = exitWest;
+		this.exitEast = exitEast;
 	}
 	
 	public String getName() {
@@ -15,5 +19,18 @@ public class Rooms {
 	
 	public String getDescription(){
 		return description;
+	}
+	
+	public int getNorthExit(){
+		return exitNorth;
+	}
+	public int getSouthExit(){
+		return exitSouth;
+	}
+	public int getWestExit(){
+		return exitWest;
+	}
+	public int getEastExit(){
+		return exitEast;
 	}
 }
