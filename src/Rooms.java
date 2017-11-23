0fd3cd7ff82f2d3a5764b6ecfe63e4f68822b871
Revodavid1 <32637870+Revodavid1@users.noreply.github.com>
2017-11-23 08:40:18 -0500

    Update Rooms.java
    Iteration 3 - scan rooms and pick submaps

--- src/Rooms.java
+++ src/Rooms.java
@@ -3,14 +3,19 @@ package lostinpandora;
 public class Rooms {
 	String name,description;
 	int exitNorth, exitSouth,exitWest,exitEast;
+	Boolean hasSubmap;
+	Boolean hasExit;
 	
-	public Rooms(String name, String description, int exitNorth, int exitSouth,int exitWest,int exitEast) {
+	public Rooms(String name, String description, int exitNorth, int exitSouth,int exitWest,int exitEast, 
+			Boolean hasSubmap) {
 		this.name = name;
 		this.description = description;
 		this.exitNorth = exitNorth;
 		this.exitSouth = exitSouth;
 		this.exitWest = exitWest;
 		this.exitEast = exitEast;
+		this.hasSubmap = hasSubmap;
+		this.hasExit = false;
 	}
 	
 	public String getName() {
